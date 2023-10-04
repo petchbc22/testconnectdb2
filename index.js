7315a2f47ec99fb85fedb8032f921d4cd6d97d29
Tayanchon <129270437+Tayanchonk@users.noreply.github.com>
2023-10-04 10:39:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var ibmdb = require('ibm_db');
 
-ibmdb.open("DRIVER={DB2};HOSTNAME=170.100.190.11;UID='metroapi';PWD='metroapi';PORT=446;PROTOCOL=TCPIP", function (err,conn) {
+ibmdb.open("DRIVER={DB2};HOSTNAME=170.100.190.11;UID=metroapi;PWD=metroapi;PORT=446;PROTOCOL=TCPIP", function (err,conn) {
   if (err) return console.log('err--->',err);
   else {
     console.log('success')
